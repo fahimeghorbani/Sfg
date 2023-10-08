e7146ad4df4557b8a5c4192139840d32d45463bb
fahimeghorbani <fahimeghorbaniii91@gmail.com>
2023-10-08 13:35:26 -0700

    phase-1-project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  theme: {
+    
+    
+    extend: {
+      screens: {
+
+        '2xl': '1200px',
+      }
+
+   
+  },
+  plugins: [],
+}
+}
